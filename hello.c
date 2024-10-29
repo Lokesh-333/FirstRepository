04d0d2c456a92cbfc1e9ce1fe5efefac482d513c
lokeshtkr5 <lokeshtkr333@gmail.com>
2024-10-29 22:03:13 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // headerfile //pre-processor directive
+
+int main(){ //main function entry point
+    printf("Hello, World!"); //Hello, World! is string //printf prints Hello, World!
+    return 0; //to stop program execution
+}
